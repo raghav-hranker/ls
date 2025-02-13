0bfad42e9cc7ebb4b8ea080e589c84e8f95ded47
raghav-hranker <raghavsharma@hranker.com>
2025-02-13 13:52:04 +0530

    remove components from page

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,7 @@ import Chat from "../components/Chat"
 export default function Home() {
   return (
     <div className="flex flex-col lg:flex-row h-screen">
-      <div className="w-full lg:w-3/4 p-4">
-        <VideoPlayer />
-      </div>
-      <div className="w-full lg:w-1/4 p-4">
-        <Chat />
-      </div>
+     
     </div>
   )
 }
